2792e6752cbd9efba3b3c4decde12cce1058fe79
Pollyana Sampaio <pollyanasampaio7@gmail.com>
2020-04-19 13:41:54 -0300

    very bad solution

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+  import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+	    int N = sc.nextInt();
+		
+		int [][] mat = new int [N] [N];
+		
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<N; j++) {
+				mat [i][j] = sc.nextInt();
+				
+			}
+		}
+
+	
+		for (int i=0; i<N; i++) {
+	           int	SOMA = 0;
+			for (int j=0; j<N; j++) {
+				SOMA = SOMA + mat [i][j];
+				}
+			System.out.println(SOMA);
+		}  
+	
+		
+	
+		sc.close();
+	}
+}
